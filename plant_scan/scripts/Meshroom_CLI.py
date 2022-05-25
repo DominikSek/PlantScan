417376a3cc0444ff1c521828ceb482678852f54a
Dominik <105292749+DominikSek@users.noreply.github.com>
2022-05-25 18:29:43 +0200

    Meshroom command line interface v2
    Optimized parameters

--- plant_scan/scripts/Meshroom_CLI.py
+++ plant_scan/scripts/Meshroom_CLI.py
@@ -6,8 +6,8 @@ import time
 from pathlib import Path
 
 dirname = os.path.dirname(os.path.abspath(__file__))  # Absolute path of this file
-
 verboseLevel = "\"" + "error" + "\""  # detail of the logs (error, info, etc)
+SIMPLIFY = False    # Set to True if needing speedup
 
 
 def SilentMkdir(theDir):    # function to create a directory
@@ -24,12 +24,12 @@ def cameraInit(binPath,baseDir,imgDir):
     SilentMkdir(baseDir + taskFolder)
     imageFolder = "\"" + imgDir + "\""
     sensorDatabase = "\"" + str(Path(binPath).parent) + "/share/aliceVision/cameraSensors.db" "\""
+    FOV = 170
    
     output = "\"" + baseDir + taskFolder + "/cameraInit.sfm" + "\""
 
     cmdLine = binPath + "aliceVision_cameraInit" + f" --imageFolder {imageFolder} --sensorDatabase {sensorDatabase} --output {output}"
-    cmdLine += " --defaultFieldOfView 45"
-    #cmdLine += " --allowSingleView 1"
+    cmdLine += f" --defaultFieldOfView {FOV}"
     cmdLine += " --verboseLevel " + verboseLevel
     os.system(cmdLine)
     return 0
@@ -39,7 +39,7 @@ def featureExtraction(binPath,baseDir , numberOfImages , imagesPerGroup=40):
 
     taskFolder = "/2_FeatureExtraction"
     SilentMkdir(baseDir + taskFolder)
-    useCPU = 0 ### Change this if CPU is needed
+    useCPU = 0  ### Change this if CPU is needed
 
 
     inputFolder = "\"" + baseDir + "/1_CameraInit/cameraInit.sfm" + "\""
@@ -83,14 +83,17 @@ def featureMatching(binPath,baseDir,numberOfImages,imagesPerGroup=40):
     output = "\"" + baseDir + taskFolder + "\""
     featuresFolders = "\"" + baseDir + "/2_FeatureExtraction" + "\""
     imagePairsList = "\"" + baseDir + "/3_ImageMatching/imageMatches.txt" + "\""
+    method = "FAST_CASCADE_HASHING_L2"  #    Feature matching method
+                                        #    Fastest but heavy on computation power
 
     cmdLine = binPath + "/aliceVision_featureMatching" + f" --input {inputFolder} --featuresFolders {featuresFolders} --output {output}"
     cmdLine += f"--imagePairsList {imagePairsList}"
     cmdLine += " --knownPosesGeometricErrorMax 5"       ##This is default in meshroom, could be changed in the future
     cmdLine += " --verboseLevel " + verboseLevel
+    cmdLine += f"--photometricMatchingMethod {method}"
 
     # These are all default parameters given by meshroom, except the debug files, that we use.
-    cmdLine += " --describerTypes sift --photometricMatchingMethod ANN_L2 --geometricEstimator acransac --geometricFilterType fundamental_matrix --distanceRatio 0.8"
+    cmdLine += " --describerTypes sift  --geometricEstimator acransac --geometricFilterType fundamental_matrix --distanceRatio 0.8"
     cmdLine += " --maxIteration 2048 --geometricError 0.0 --maxMatches 0"
     cmdLine += " --savePutativeMatches False --guidedMatching False --matchFromKnownCameraPoses False --exportDebugFiles True"
 
@@ -115,14 +118,14 @@ def structureFromMotion(binPath, baseDir):
     extraInfoFolder = "\"" + baseDir + taskFolder + "\""
     featuresFolders = "\"" + baseDir + "/2_FeatureExtraction" + "\""
     matchesFolders = "\"" + baseDir + "/4_featureMatching" + "\""
-
+    estimator = "acransac"  #   Estimator that adapts to noise
     cmdLine = binPath + "/aliceVision_incrementalSfM" + f" --input {inputFolder} --output {output}"
 
     # Necessary folders to export data
     # It pulls all the parameters given in featureMatching()
     cmdLine += f"--outputViewsAndPoses {outputViewsAndPoses} --extraInfoFolder {extraInfoFolder} --featuresFolders {featuresFolders} --matchesFolders {matchesFolders}"
     cmdLine += " --verboseLevel " + verboseLevel
-
+    cmdLine += f"--localizerEstimator {estimator}"
     os.system(cmdLine)
 
 
@@ -151,8 +154,9 @@ def depthMap(binPath, baseDir, numberOfImages, groupSize=6, downscale=2):
 
     # Downscale = 2 is the default value given by meshroom
     cmdLine += " --downscale " + str(downscale)
+    cmdLine += "--nbGPUs 0"     #   0 == Use all my GPU's
 
-    # Splitting groups again (found on github)
+    # Splitting groups again
     for i in range(int(math.ceil(numberOfImages / groupSize))):
         groupStart = groupSize * i
         if groupSize > 1:
@@ -171,7 +175,7 @@ def depthMapFilter(binPath,baseDir):
 
     cmdLine = binPath + "/aliceVision_depthMapFiltering" + f" --input {inputFolder}  --output {output} --depthMapsFolder {depthMapsFolder}"
     cmdLine += " --verboseLevel " + verboseLevel
-
+    cmdLine += "--nNearestCams 10"
     os.system(cmdLine)
 
 
@@ -189,11 +193,12 @@ def meshing(binPath, baseDir, maxInputPoints = 50000000, maxPoints=5000000):
     cmdLine += " --maxInputPoints " + str(maxInputPoints)
     cmdLine += " --maxPoints " + str(maxPoints)
     cmdLine += " --verboseLevel " + verboseLevel
+    cmdLine += "--estimateSpaceFromSfM true"
 
     os.system(cmdLine)
 
 
-def meshFiltering(binPath, baseDir, keepLargestMeshOnly="True"):
+def meshFiltering(binPath, baseDir, keepLargestMeshOnly="false"):
     taskFolder = "/10_MeshFiltering"
     SilentMkdir(baseDir + taskFolder)
 
@@ -207,7 +212,7 @@ def meshFiltering(binPath, baseDir, keepLargestMeshOnly="True"):
     os.system(cmdLine)
 
 
-def meshDecimate(binPath,baseDir , simplificationFactor=0.8 , maxVertices=15000):
+def meshDecimate(binPath,baseDir , simplificationFactor=0.8):
     taskFolder = "/11_MeshDecimate"
     SilentMkdir(baseDir + taskFolder)
 
@@ -218,43 +223,44 @@ def meshDecimate(binPath,baseDir , simplificationFactor=0.8 , maxVertices=15000)
 
     cmdLine += " --verboseLevel " + verboseLevel
     cmdLine += " --simplificationFactor " + str(simplificationFactor)
-    #cmdLine += " --maxVertices " + str(maxVertices)
 
     os.system(cmdLine)
 
 
-def meshResampling(binPath,baseDir , simplificationFactor=0.8 , maxVertices=15000):
+def meshResampling(binPath,baseDir , simplificationFactor=0.8):
     taskFolder = "/12_MeshResampling"
     SilentMkdir(baseDir + taskFolder)
 
-    inputMesh = "\"" + baseDir +  "/11_MeshDecimate/mesh.obj" + "\""
-    outputMesh = "\"" + baseDir  + taskFolder + "/mesh.obj" + "\""
+    inputMesh = "\"" + baseDir + "/11_MeshDecimate/mesh.obj" + "\""
+    outputMesh = "\"" + baseDir + taskFolder + "/mesh.obj" + "\""
 
     cmdLine = binPath + "/aliceVision_meshResampling" + f" --input {inputMesh}  --output {outputMesh}"
     cmdLine += " --verboseLevel " + verboseLevel
     cmdLine += " --simplificationFactor " + str(simplificationFactor)
-    #cmdLine += " --maxVertices " + str(maxVertices)
 
     os.system(cmdLine)
 
 
-def texturing(binPath, baseDir, finish_Dir, textureSide = 4096 , downscale=4, unwrapMethod = "Basic", meshDecAndResample=True):
+def texturing(binPath, baseDir, textureSide = 8192 , downscale=2, unwrapMethod = "Basic"):
     taskFolder = "/Finished_texture"
-    SilentMkdir(finish_Dir + taskFolder)
+    SilentMkdir(baseDir + taskFolder)
 
     inputFolder = "\"" + baseDir + "/9_Meshing/densePointCloud.abc" + "\""
     imagesFolder = "\"" + baseDir + "/6_PrepareDenseScene" "\""
-    if meshDecAndResample:
+    if not SIMPLIFY:
         inputMesh = "\"" + baseDir + "/12_MeshResampling/mesh.obj" + "\""
     else:
         inputMesh = "\"" + baseDir + "/10_MeshFiltering/mesh.obj" + "\""
-    output = "\"" + finish_Dir + taskFolder + "\""
+    output = "\"" + baseDir + taskFolder + "\""
 
     cmdLine = binPath + "/aliceVision_texturing" + f" --input {inputFolder} --inputMesh {inputMesh} --output {output} --imagesFolder {imagesFolder}"
     cmdLine += " --textureSide " + str(textureSide)
     cmdLine += " --downscale " + str(downscale)
     cmdLine += " --verboseLevel " + verboseLevel
+
+    ### Unwrap method can be LSCM or ABF if we decide to Simplify our mesh
     cmdLine += " --unwrapMethod " + unwrapMethod
+    cmdLine += " --padding 5"   ## Increase this if needed
 
     os.system(cmdLine)
 
@@ -262,7 +268,8 @@ def texturing(binPath, baseDir, finish_Dir, textureSide = 4096 , downscale=4, un
 def count_images(imgDir):
     return len([name for name in os.listdir(imgDir) if os.path.isfile(os.path.join(imgDir, name))])   ## number of files in the folder
 
-def __run__(binPath,baseDir,imgDir, finish_Dir):
+
+def __run__(binPath,baseDir,imgDir):
     numberOfImages = count_images(imgDir)
     SilentMkdir(baseDir)
 
@@ -278,23 +285,16 @@ def __run__(binPath,baseDir,imgDir, finish_Dir):
     meshFiltering(binPath, baseDir)
     meshDecimate(binPath, baseDir, simplificationFactor=0.5)
     meshResampling(binPath, baseDir, simplificationFactor=0.5)
-    texturing(binPath, baseDir, finish_Dir, downscale=2, textureSide=8192)
+    texturing(binPath, baseDir, downscale=2, textureSide=8192)
 
-def main():
+
+if __name__ == "__main__":
 
     # Pass the arguments of the function as parameters in the command line code
     binPath = sys.argv[1]           ##  --> path of the binary files from Meshroom
     baseDir = sys.argv[2]           ##  --> name of the Folder containing the process (a new folder will be created), metadata
     imgDir = sys.argv[3]            ##  --> Folder containing the images
-    if len(sys.argv) == 5:
-        finish_Dir = sys.argv[4]        ##  --> Folder with finished texture
-    else:
-        finish_Dir = baseDir
-    
-    __run__(binPath, baseDir, imgDir, finish_Dir)
+
+    __run__(binPath, baseDir, imgDir)
     print("-------------------- DONE --------------------")
-
-    #raw_input("press any key to close")
-
-
-main()
+    exit()
